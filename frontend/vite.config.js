d639921cc0fb91032c99706342fe78d579309d65
PhamGiaKhoi <phamgiakhoi0123@gmail.com>
2025-11-09 00:33:44 +0700

    fix: Use base path only in production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/eduverse-hackathon/', // Tên repo GitHub
+  base: process.env.NODE_ENV === 'production' ? '/eduverse-hackathon/' : '',
   server: {
     port: 5173,
     proxy: {
